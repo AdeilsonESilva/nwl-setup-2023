fff598c52f79871a0615e0054f6abb67ad44d8d9
Adeilson Silva <adeilson.silva@brainweb.com.br>
2023-01-19 07:41:13 -0300

    third class

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,7 +9,6 @@ export async function appRoute(app: FastifyInstance) {
       title: z.string(),
       weekDays: z.array(z.number().min(0).max(6)),
     });
-
     const { title, weekDays } = createHabitBody.parse(request.body);
     const today = dayjs().startOf("day").toDate();
 
@@ -30,7 +29,6 @@ export async function appRoute(app: FastifyInstance) {
     const getDayParams = z.object({
       date: z.coerce.date(),
     });
-
     const { date } = getDayParams.parse(request.query);
     const parsedDate = dayjs(date).startOf("day");
     const weekDay = parsedDate.get("day");
@@ -63,5 +61,78 @@ export async function appRoute(app: FastifyInstance) {
       possibleHabits,
       completedHabits,
     };
+  });
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+    const { id } = toggleHabitParams.parse(request.params);
+    const today = dayjs().startOf("day").toDate();
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
+
+  app.get("/summary", async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        d.id, 
+        d.date,
+        (
+          SELECT 
+            cast(count(1) as float)
+          FROM day_habits dh
+          WHERE 
+            dh.day_id = d.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(1) as float)
+          FROM habit_week_days hwd
+          JOIN habits h
+          ON h.id = hwd.habit_id
+          WHERE 
+            hwd.week_day = cast(strftime('%w', d.date/1000, 'unixepoch') as int)
+            AND h.created_at <= d.date
+        ) as amount
+      FROM days d
+    `;
+
+    return summary;
   });
 }
